d0edeacfc1933d0376c9eb0b717417e26aa34d14
gdinit <gdinit@gmx.com>
2017-09-27 21:53:04 +0100

    add round() implementation

---
+++ ch01/round.h
@@ -0,0 +1,21 @@
+/* round.h */
+
+/*
+	ANSI C round() implementation copyright notice
+
+	(c) Barbara Carter  April 12, 1995 
+	The Dunham telescope control program version 1.1
+
+	Retrieved on	: 2017-09-27
+	Retrieved fro	: GitHub user page Mitch Spradlin
+			  https://github.com/mhspradlin/DUNHAM
+*/
+
+/* ===================================80 chars=============================== */
+
+long round( double number );
+
+/* ===================================80 chars=============================== */
+
+/* EOF */
+
