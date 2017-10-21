ec8ba0a0b9ecdaabf092b67f9fe178c71d664326
gdinit <gdinit@gmx.com>
2017-10-21 23:12:22 +0100

    Add Chapter 05 - Programming Exercise 13

---
+++ ch05/05pe13.c
@@ -0,0 +1,104 @@
+/* 05pe13.c */
+
+/* ===================================80 chars=============================== */
+
+/*
+Write a program to compute and display the sum of all integers that are
+divisible by 6 but not by 4 and lie between 0 and 100. The program should also
+count and display the number of such values.
+*/
+
+/*
+Compiled & tested with:
+gcc -std=c89 -pedantic -Wall -Werror $filename.c -o binary/$filename
+*/
+
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*
+Required to prevent -> "scanf: floating point formats not linked"
+*/
+#ifdef _WIN32
+#elif defined __unix__
+#elif defined __APPLE__
+#else
+extern unsigned _doubleconvert;
+#pragma extref _doubleconvert
+#endif
+
+/* CONFIGURATION */
+#define TITLE "NUMBERS BETWEEN 0 and 100, DIVISIBLE BY 6 BUT NOT 4"
+#define LINES_TO_CLEAR_SCREEN 90
+#define DASH_COUNT_FOR_HEADER_FOOTER 66
+
+void cls( void )
+{
+	int n;
+	for ( n = 0; n < LINES_TO_CLEAR_SCREEN; n++ ) {
+		printf( "\n" );
+	}
+}
+
+void displayHeader( void )
+{
+	char	s [] = TITLE;
+	int	tmp = 0;
+	for ( tmp = 0; tmp < DASH_COUNT_FOR_HEADER_FOOTER; tmp++ ) {
+		printf( "-" );
+	}
+	printf( "\n%s\n\n", s );
+}
+
+void displayFooter( void )
+{
+	int tmp = 0;
+	for ( tmp = 0; tmp < DASH_COUNT_FOR_HEADER_FOOTER; tmp++ ) {
+		printf( "-" );
+	}
+	printf( "\n" );
+}
+
+void computeDisplay( void )
+{
+	int count = 0, n = 0, sum = 0;
+	for ( n = 0; n < 100; n++ ) {
+		if ( n % 6 == 0 && n % 4 != 0 ) {
+			count++;
+			sum += n;
+			printf( "Number %2d is divisible by 6 and not by 4.\t"
+				"Count %2d | Sum %2d\n", n, count, sum );
+		}
+	}
+}
+
+int main( void )
+{
+	/* BEGIN: Declare Variables ***************************************** */
+	char ch = '\0';
+	/* END: Declare Variables ******************************************* */
+
+	/* BEGIN: Program Main Code ***************************************** */
+	cls();
+	displayHeader();
+	computeDisplay();
+	displayFooter();
+	/* END: Program Main Code ******************************************* */
+
+	/* BEGIN: Standard Footer Section *********************************** */
+	printf( "\nPress space to quit\n" );
+	/* disable input buffer */
+	setvbuf( stdin,	0, _IONBF, 0 );
+	while ( ( ch = getchar() ) != ' ' && ch != EOF ) {
+	}
+	/* END: Standard Footer Section ************************************* */
+
+	return 0;
+}
+
+/* ===================================80 chars=============================== */
+
+/* EOF */
+
