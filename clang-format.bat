d0a8e05c70e62750b0aa659557772750ba8229c5
gdinit <gdinit@gmx.com>
2018-04-24 15:41:13 +0100

    Add a batch script to launch clang-format

---
+++ clang-format.bat
@@ -0,0 +1,67 @@
+:: Find all files matching a given extension in a specific subdirectory ('ch01')
+:: and clang-format them in-place.
+@ECHO OFF
+
+:: Provide initial feedback
+ECHO ****************************************
+ECHO BEGIN clang-formatting
+
+:: Chapter 01
+ECHO.
+CD ch01
+FOR %%* in (.) DO ECHO STARTED Processing: %%~nx*
+FOR %%f in (*.c *.h) DO clang-format -style=file -i %%f
+FOR %%* in (.) DO ECHO COMPLETED Processing: %%~nx*
+CD ..
+ECHO.
+
+:: Chapter 02
+ECHO.
+CD ch02
+FOR %%* in (.) DO ECHO STARTED Processing: %%~nx*
+FOR %%f in (*.c *.h) DO clang-format -style=file -i %%f
+FOR %%* in (.) DO ECHO COMPLETED Processing: %%~nx*
+CD ..
+ECHO.
+
+:: Chapter 03
+ECHO.
+CD ch03
+FOR %%* in (.) DO ECHO STARTED Processing: %%~nx*
+FOR %%f in (*.c *.h) DO clang-format -style=file -i %%f
+FOR %%* in (.) DO ECHO COMPLETED Processing: %%~nx*
+CD ..
+ECHO.
+
+:: Chapter 04
+ECHO.
+CD ch04
+FOR %%* in (.) DO ECHO STARTED Processing: %%~nx*
+FOR %%f in (*.c *.h) DO clang-format -style=file -i %%f
+FOR %%* in (.) DO ECHO COMPLETED Processing: %%~nx*
+CD ..
+ECHO.
+
+:: Chapter 05
+ECHO.
+CD ch05
+FOR %%* in (.) DO ECHO STARTED Processing: %%~nx*
+FOR %%f in (*.c *.h) DO clang-format -style=file -i %%f
+FOR %%* in (.) DO ECHO COMPLETED Processing: %%~nx*
+CD ..
+ECHO.
+
+:: Chapter 06
+ECHO.
+CD ch06
+FOR %%* in (.) DO ECHO STARTED Processing: %%~nx*
+FOR %%f in (*.c *.h) DO clang-format -style=file -i %%f
+FOR %%* in (.) DO ECHO COMPLETED Processing: %%~nx*
+CD ..
+ECHO.
+
+:: Provide final feedback
+ECHO END clang-formatting
+ECHO ****************************************
+
+:: EOF
