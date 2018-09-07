2ea57e89a1e7d5e8f8715d9904d9d1bad95cdc27
gdinit <gdinit@gmx.com>
2018-09-07 23:14:01 +0100

    Add Chapter 12 - Programming Exercise 01

---
+++ ch12/12pe01.c
@@ -0,0 +1,118 @@
+/* 12pe01.c */
+/* ===================================80 chars=============================== */
+
+/*
+ * Write a program to copy the contents of one file to another.
+ */
+
+/*
+ * Compiled & tested on:
+ *	Apple LLVM version 9.1.0 (clang-902.0.39.1)
+ *	Target: x86_64-apple-darwin17.5.0
+ * with:
+ * 	clang -x c -std=c89 -pedantic-errors -Wall -Werror -Wextra -Wcomment \
+ *	-Wparentheses -Wformat-zero-length 12pe01.c -o binary/12pe01
+ */
+
+/* KNOWN ISSUES
+ * Works only with text files!
+ */
+
+/* Needed to stop annoying MS _s warnings when compiled with llvm on Windows! */
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <ctype.h> /* tolower() */
+#include <stdio.h>
+#include <stdlib.h> /* exit(), EXIT_FAILURE, strtol(), qsort() */
+#include <string.h> /* for title */
+
+/* CONFIGURATION */
+#define LINES_TO_CLEAR_SCREEN 90
+#define DASH_COUNT_FOR_HEADER_FOOTER 66
+#define STR(x) #x
+#define XSTR(x) STR(x)
+/**************************************/
+#define TITLE "FILE COPY"
+#define CFGDEBUG 1
+#define SOURCE_FILE "source.txt"
+#define DESTINATION_FILE "destination.txt"
+
+/* BEGIN: FLUSH */
+#ifdef _WIN32
+#define FLUSH fflush(stdin);
+#elif defined __unix__
+#define FLUSH fpurge(stdin);
+#elif defined __APPLE__
+#define FLUSH fpurge(stdin);
+#else
+#define FLUSH fflush(stdin);
+#endif
+/* END: FLUSH */
+
+void cls(void);
+void displayHeaderLine(void);
+void displayHeaderText(void);
+void displayFooter(void);
+/**************************************/
+
+/* PROGRAM ENTRY POINT */
+int main(void)
+{
+	FILE *fpsrc, *fpdst;
+	char c;
+
+	cls();
+	displayHeaderLine();
+	displayHeaderText();
+	printf("\n");
+
+	fpsrc = fopen(SOURCE_FILE, "r");
+	fpdst = fopen(DESTINATION_FILE, "w");
+
+	/* read the file till we reach the end */
+	while ((c = getc(fpsrc)) != EOF) {
+		putc(c, fpdst);
+	}
+	printf("File copy completed!\n");
+
+	displayFooter();
+
+	return EXIT_SUCCESS;
+}
+
+/**************************************/
+void cls(void)
+{
+	int n;
+	for (n = 0; n < LINES_TO_CLEAR_SCREEN; n++) {
+		printf("\n");
+	}
+}
+
+void displayHeaderLine(void)
+{
+	int n;
+	for (n = 0; n < DASH_COUNT_FOR_HEADER_FOOTER; n++) {
+		printf("-");
+	}
+	printf("\n");
+}
+
+void displayHeaderText(void)
+{
+	char s[] = TITLE;
+	printf("%s\n", s);
+}
+
+void displayFooter(void)
+{
+	int n = 0;
+	for (n = 0; n < DASH_COUNT_FOR_HEADER_FOOTER; n++) {
+		printf("-");
+	}
+	printf("\n");
+}
+/**************************************/
+
+/* ===================================80 chars=============================== */
+/* EOF */
