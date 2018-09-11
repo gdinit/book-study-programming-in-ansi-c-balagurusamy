240c3d328c862ffc70e7818d271f4bcd6ba400aa
gdinit <gdinit@gmx.com>
2018-09-11 22:56:52 +0100

    Modify Chapter 11 FLUSH fix

--- ch11/11pe09.c
+++ ch11/11pe09.c
@@ -32,17 +32,20 @@
 #define TITLE "FUNCTION THAT REVERSES THE ELEMENTS OF A GIVEN ARRAY"
 #define CFGDEBUG 1
 
-/* BEGIN: FLUSH */
+/* BEGIN: FLUSH rev.03 */
 #ifdef _WIN32
 #define FLUSH fflush(stdin);
-#elif defined __unix__
-#define FLUSH fpurge(stdin);
+/*#elif defined __unix__
+#define FLUSH fpurge(stdin);*/
 #elif defined __APPLE__
 #define FLUSH fpurge(stdin);
+#elif defined __gnu_linux__
+#include <stdio_ext.h>
+#define FLUSH __fpurge(stdin);
 #else
 #define FLUSH fflush(stdin);
 #endif
-/* END: FLUSH */
+/* END: FLUSH rev.03 */
 
 void cls(void);
 void displayHeaderLine(void);
